f43902d71f579058b6ebe6951525c82cac0206a9
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-08-30 21:08:17 +0700

    Refactor. Views.py. Move repeated responses to variables.

--- views/views.py
+++ views/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from cgi import escape
 from jinja2 import Environment, PackageLoader, select_autoescape
+from minimaluwsgi import env
+
+response_OK = ('200 OK', [('Content-Type', 'text/html')])
+response_NOT_FOUND = ('404 NOT FOUND', [('Content-Type', 'text/html')])
 
 
 def hello(environ, start_response):
@@ -10,15 +14,11 @@ def hello(environ, start_response):
         subject = escape(args[0])
     else:
         subject = 'World'
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    start_response(*response_OK)
     return ['Hello %(subject)s' % {'subject': subject}]
 
 
 def joy(environ, start_response):
-    env = Environment(
-        loader=PackageLoader('minimaluwsgi', 'templates'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
     template = env.get_template('joy.html')
     args = environ['minimaluwsgi.url_args']
     if args:
@@ -27,18 +27,18 @@ def joy(environ, start_response):
         passed_name = u'World'
     args2 = environ.get('QUERY_STRING', '')
     passed_args = args2 if args2 else u'Ничего!'
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    start_response(*response_OK)
     r_val = {'pname': passed_name, 'pargs': passed_args}
     return template.render(r_val).encode('utf-8')
 
 
 def index(environ, start_response):
     """This function will be mounted on "/"."""
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    start_response(*response_OK)
     return ['minimaluwsgi Application']
 
 
 def not_found(environ, start_response):
     """Called if no URL matches."""
-    start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
+    start_response(*response_NOT_FOUND)
     return ['Not Found']
