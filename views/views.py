a6b59956b6fa9020217fed838f48aa0c16fe08ca
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2018-08-31 21:13:25 +0700

    Refactor view.py. Add error handling

--- views/views.py
+++ views/views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from cgi import escape
-from jinja2 import Environment, PackageLoader, select_autoescape
 from minimaluwsgi import env
 
-response_OK = ('200 OK', [('Content-Type', 'text/html')])
-response_NOT_FOUND = ('404 NOT FOUND', [('Content-Type', 'text/html')])
+responses = {
+    200: ('200 OK', [('Content-Type', 'text/html')]),
+    404: ('404 NOT FOUND', [('Content-Type', 'text/html')])
+}
 
 
 def hello(environ, start_response):
@@ -14,7 +15,7 @@ def hello(environ, start_response):
         subject = escape(args[0])
     else:
         subject = 'World'
-    start_response(*response_OK)
+    start_response(*responses[200])
     return ['Hello %(subject)s' % {'subject': subject}]
 
 
@@ -22,23 +23,26 @@ def joy(environ, start_response):
     template = env.get_template('joy.html')
     args = environ['minimaluwsgi.url_args']
     if args:
-        passed_name = escape(args[0])
+        try:
+            passed_name = escape(args[0])
+        except IndexError:
+            passed_name = u'Strange World!'
     else:
         passed_name = u'World'
     args2 = environ.get('QUERY_STRING', '')
     passed_args = args2 if args2 else u'Ничего!'
-    start_response(*response_OK)
+    start_response(*responses[200])
     r_val = {'pname': passed_name, 'pargs': passed_args}
     return template.render(r_val).encode('utf-8')
 
 
 def index(environ, start_response):
     """This function will be mounted on "/"."""
-    start_response(*response_OK)
+    start_response(*responses[200])
     return ['minimaluwsgi Application']
 
 
 def not_found(environ, start_response):
     """Called if no URL matches."""
-    start_response(*response_NOT_FOUND)
+    start_response(*responses[404])
     return ['Not Found']
